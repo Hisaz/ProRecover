1728224a45d0715401706dd82d64ab6790690e22
diogopelaes <diogopelaes@gmail.com>
2024-05-09 11:39:42 -0300

    Init2

---
+++ src/main/java/com/tcc/ProRecover/ProRecoverApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.ProRecover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProRecoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProRecoverApplication.class, args);
+	}
+
+}
